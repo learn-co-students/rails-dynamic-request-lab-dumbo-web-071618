ad495e2c10bcc3769ba90868bdcbfb898e158406
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-10 08:58:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  # Use square brackets ([]) to house multiple RESTful actions for "only":
+  # https://stackoverflow.com/questions/6082929/limit-resources-actions
+  resources :students, only: [:index, :show]
 end
